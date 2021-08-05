aa9dfedf0ecae2339e36d31f25b1af80f739e91c
Kana Otawara <shimmer22.0204@gmail.com>
2021-08-06 00:43:16 +0900

    change color of qr code

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,8 +1,9 @@
 package example
 
 import com.google.zxing.BarcodeFormat
-import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.client.j2se.{MatrixToImageConfig, MatrixToImageWriter}
 import com.google.zxing.qrcode.QRCodeWriter
+
 import javax.imageio.ImageIO
 import java.nio.file.Files
 import java.nio.file.Paths
@@ -11,7 +12,9 @@ import java.nio.file.Paths
 object Hello extends Greeting with App {
 
   val matrix = new QRCodeWriter().encode("https://www.google.com/", BarcodeFormat.QR_CODE, 320, 320)
-  val qrCodeImage = MatrixToImageWriter.toBufferedImage(matrix)
+
+  val config = new MatrixToImageConfig(0xff0000ff, 0xffffffff);
+  val qrCodeImage = MatrixToImageWriter.toBufferedImage(matrix, config)
   ImageIO.write(qrCodeImage, "png", Files.newOutputStream(Paths.get("/Users/kana/Downloads/qrcode.png")))
 }
 
