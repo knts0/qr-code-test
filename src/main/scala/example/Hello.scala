32533f5c2da384fd0c88028082e78dc0988bd792
Kana Otawara <shimmer22.0204@gmail.com>
2021-08-06 00:33:23 +0900

    implement generation of qr code

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,7 +1,18 @@
 package example
 
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.qrcode.QRCodeWriter
+import javax.imageio.ImageIO
+import java.nio.file.Files
+import java.nio.file.Paths
+
+
 object Hello extends Greeting with App {
-  println(greeting)
+
+  val matrix = new QRCodeWriter().encode("https://www.google.com/", BarcodeFormat.QR_CODE, 320, 320)
+  val qrCodeImage = MatrixToImageWriter.toBufferedImage(matrix)
+  ImageIO.write(qrCodeImage, "png", Files.newOutputStream(Paths.get("/Users/kana/Downloads/qrcode.png")))
 }
 
 trait Greeting {
